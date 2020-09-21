76f20bad7ab195a1303555ef6fce7aa5821ff297
Anegasaki <vienxtras@gmail.com>
2020-09-20 20:48:13 -0400

    added images
    Would it have been smarter to just have had it handle finding the right file by naming them all the same thing as their variable name? Yes. Am I very not smart and susceptible to the sunk cost fallacy? Yes. Did I end up renaming them to be the variables anyway in case I care enough to fix it? Yes.

--- code.js
+++ code.js
@@ -1,34 +1,35 @@
 let organelles = [];
 let mode = "Name";
 class ornel {
-	constructor(name, anat, phys){
+	constructor(name, anat, phys, img){
 		this.name = name;
 		this.anat = anat;
 		this.phys = phys;
+		this.img = img
 		organelles.push(this);
 	}
 }
 
-const plasm = new ornel("Plasma Membrane", "Phospholipid Bilayer, Pro - Double, Eu - Single", "The outer border of the cell (border control)");
-const nucls = new ornel("Nucleus", "Next largest after central vacuole", "Cell's control center");
-const envel = new ornel("Nuclear Envelope", "Around the nucleus, connected to R.ER", "Separates nucleus internals from externals");
-const chrom = new ornel("Chromatin", "Like rope, in the nucleus", "Compressed DNA");
-const nuclo = new ornel("Nucleolus", "In the nucleus, round", "Regulates cell functions & proteins");
-const cvac = new ornel("Central Vacuole", 'Very large, "empty", even pushes nucleus into wall, in plant cells', "Warehouse");
-const svac = new ornel("Small Vacuole", 'Smaller than central, "empty"', "Storage shed");
-const ser = new ornel("Smooth ER", "Like coral", "Carb & Lipid factory, cell maintenence");
-const rer = new ornel("Rough ER", "Waves w/ dots, close to nucleus", "Protein factory");
-const ribo = new ornel("Ribosome", "Small circles, may be on R.ER", "RNA Translators");
-const cytol = new ornel("Cytosol", "The actual substance everything sits in", "Keeps everything in place, has variable consistency");
-const lyso = new ornel("Lysosome", "Has dots inside", "Converts larger food molecules into cell-usable form");
-const mito = new ornel("Mitochondria", "Lines going across inside", "ATP generator (fueled by Lyso)");
-const chloro = new ornel("Chloroplast", "Has stacked discs", "Photosynthesis");
-const golgi = new ornel("Golgi Apparatus", "Like loops of bacon", "Protein USPS");
-const cytok = new ornel("Cytoskeleton", "Like a web", "Organises and moves the cell and the stuff inside");
-const centri = new ornel("Centrioles", "Like a bamboo pillar", "Anchors during mitosis");
-const wall = new ornel("Cell Wall", "Outside of the membrane, made of cellulose, in plant cells", "More rigid structural support, helps keep more water in");
-const flag = new ornel("Flagella", "Like whips", "Movement, sometimes sensor");
-const cil = new ornel("Cilia", "Like hairs", "Help with movement (either things around it or the cell itself)");
+const plasm = new ornel("Plasma Membrane", "Phospholipid Bilayer, Pro - Double, Eu - Single", "The outer border of the cell (border control)", "plasma.png");
+const nucls = new ornel("Nucleus", "Next largest after central vacuole", "Cell's control center","nucleus.png");
+const envel = new ornel("Nuclear Envelope", "Around the nucleus, connected to R.ER", "Separates nucleus internals from externals","envel.png");
+const chrom = new ornel("Chromatin", "Like rope, in the nucleus", "Compressed DNA","chroma.png");
+const nuclo = new ornel("Nucleolus", "In the nucleus, round", "Regulates cell functions & proteins","nuclo.png");
+const cvac = new ornel("Central Vacuole", 'Very large, "empty", even pushes nucleus into wall, in plant cells', "Warehouse","cvac.png");
+const svac = new ornel("Small Vacuole", 'Smaller than central, "empty"', "Storage shed","svac.png");
+const ser = new ornel("Smooth ER", "Like coral", "Carb & Lipid factory, cell maintenence","ser.png");
+const rer = new ornel("Rough ER", "Waves w/ dots, close to nucleus", "Protein factory","rer.png");
+const ribo = new ornel("Ribosome", "Small circles, may be on R.ER", "RNA Translators","ribo.png");
+const cytol = new ornel("Cytosol", "The actual substance everything sits in", "Keeps everything in place, has variable consistency","cyto.png");
+const lyso = new ornel("Lysosome", "Has dots inside", "Converts larger food molecules into cell-usable form","lyso.png");
+const mito = new ornel("Mitochondria", "Lines going across inside", "ATP generator (fueled by Lyso)","mitchondria.png");
+const chloro = new ornel("Chloroplast", "Has stacked discs", "Photosynthesis","chloro.png");
+const golgi = new ornel("Golgi Apparatus", "Like loops of bacon", "Protein USPS","golgi.png");
+const cytok = new ornel("Cytoskeleton", "Like a web", "Organises and moves the cell and the stuff inside","cytok.png");
+const centri = new ornel("Centrioles", "Like a bamboo pillar", "Anchors during mitosis","centri.png");
+const wall = new ornel("Cell Wall", "Outside of the membrane, made of cellulose, in plant cells", "More rigid structural support, helps keep more water in","wall.png");
+const flag = new ornel("Flagella", "Like whips", "Movement, sometimes sensor","flag.png");
+const cil = new ornel("Cilia", "Like hairs", "Help with movement (either things around it or the cell itself)","cil.png");
 
 let order = [];
 for (i=0;i<20;++i) order[i]=i;
@@ -50,27 +51,30 @@ function shuffle(){
 	current = 0;
 	for (let pos of order){
 		card = document.getElementById(String(current));
+		name = organelles[pos].name;
+		anato = `${organelles[pos].anat}<br><img height="100" src=${organelles[pos].img} title='${organelles[pos].anat}'>`;
+		physi = organelles[pos].phys;
 		switch(mode){
 			case "Name":
-				card.innerHTML = organelles[pos].name;
+				card.innerHTML = name;
 				break
 			case "Anatomy":
-				card.innerHTML = organelles[pos].anat;
+				card.innerHTML = anato;
 				break
 			case "Physiology":
-				card.innerHTML = organelles[pos].phys;
+				card.innerHTML = physi;
 				break
 			case "Random":
 				let display = Math.floor(Math.random() * 3);
 				switch(display) {
 					case 0:
-						card.innerHTML = organelles[pos].name;
+						card.innerHTML = name;
 						break
 					case 1:
-						card.innerHTML = organelles[pos].anat;
+						card.innerHTML = anato;
 						break
 					case 2:
-						card.innerHTML = organelles[pos].phys;
+						card.innerHTML = physi;
 						break
 					default:
 						alert("Something's gone wrong, please press F12, click on console. and email vienxtras@gmail.com what you see!");
@@ -97,6 +101,7 @@ function flip(toFlip){
 	document.getElementById("name").innerHTML = `Name: ${selected.name}`;
 	document.getElementById("anat").innerHTML = `Anatomy: ${selected.anat}`;
 	document.getElementById("phys").innerHTML = `Physiology: ${selected.phys}`;
+	document.getElementById("focusImg").innerHTML = `<img height="200" src=${selected.img} title='${selected.anat}'>`
 }
 
 function hide(){
